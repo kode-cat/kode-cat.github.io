62f3356c7211caca7cba3451e06f7dabc2c67970
Aqib <aqibafreen12@gmail.com>
2024-12-29 12:55:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Responsive Menu Toggle
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("active");
+});
+
+// Highlight All Code Blocks
+hljs.highlightAll();
